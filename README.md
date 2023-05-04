d5252bf716bfd242701e43556824c6487698383a
Victor Kosgei <victorkosgei254@gmail.com>
2023-05-04 11:07:44 +0300

    Counter App

---
+++ README.md
@@ -0,0 +1,12 @@
+## Counter Application
+
+#### Description
+> This is a counter application that receives a number to increment the counter
+> The number should be less than 100 and greater than - 100
+>
+
+**Widgets Involved**
+
+* Button
+* EditText
+* TextView
--- app/src/main/java/com/kritbit/counter_app/MainActivity.java
+++ app/src/main/java/com/kritbit/counter_app/MainActivity.java
@@ -3,12 +3,45 @@ package com.kritbit.counter_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText increments;
+    TextView results;
+    Button   play;
+    Integer incs, res, disp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        increments = findViewById(R.id.editTextNumber);
+        results = findViewById(R.id.counter_text);
+        play = findViewById(R.id.play_btn);
+
+        play.setOnClickListener(view -> {
+            if (increments.getText().toString() == null)
+            {
+                incs = 0;
+            }else {
+                incs = Integer.parseInt(increments.getText().toString());
+            }
+            res = Integer.parseInt(results.getText().toString());
+            if (incs > 100 || incs < -100)
+            {
+                Toast.makeText(MainActivity.this,"Increments should not be greater than 100 or less than -100", Toast.LENGTH_LONG).show();
+            }
+            else {
+                disp = incs + res;
+                results.setText(disp.toString());
+            }
+
+        });
+
     }
+
 }
